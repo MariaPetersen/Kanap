c44b2f27542bc59552440779034e6d3735de9d40
Maria Petersen <mau.petersen@gmail.com>
2022-12-30 18:37:49 +0100

    add basket

--- front/js/product.js
+++ front/js/product.js
@@ -1,24 +1,18 @@
+
+//DOM elements for adding product details to page
 const itemImage = document.querySelector(".item__img")
 const productTitle = document.getElementById("title")
 const productPrice = document.getElementById("price")
 const productDescription = document.getElementById("description")
 const productColors = document.getElementById("colors")
 
-
+//Retrieving product ID from URL to identify the right product for the page
 const url = window.location.href
 const productURL = new URL(url)
 const $productId = productURL.searchParams.get('id')
 
-
-
-console.log($productId)
-
-
-itemImage.innerHTML = "<img src='../images/logo.png>'' alt='Photographie d'un canapé'"
-
+//Retrieving data from API and adding it to DOM
 function retrieveProductData() {
-
-
     fetch("http://localhost:3000/api/products")
     .then(function(response){
         if(response.ok){
@@ -29,6 +23,7 @@ function retrieveProductData() {
         product = productData.find(function(product){product = product._id === $productId; return product})
         
         itemImage.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}"/>`
+        
         productTitle.innerHTML = product.name
         
         productPrice.innerHTML = product.price 
@@ -48,3 +43,27 @@ function retrieveProductData() {
 }
 
 retrieveProductData()
+
+productColors.addEventListener('change', function(e){
+    document.querySelector('option').setAttribute("value", productColors.value)
+    e.preventDefault()
+})
+
+//DOM elements for add to cart 
+const addButton = document.getElementById("addToCart")
+const itemQuantity = document.getElementById("quantity")
+
+//Cart object
+let ProductInstance = [
+    ["", "" , 0]
+]
+
+//Add to cart functions
+console.log(ProductInstance.id)
+addButton.addEventListener("click", 
+function addToCart(i){
+   ProductInstance.push ([$productId, productColors.value, itemQuantity.value])
+    console.log(ProductInstance)
+    i.preventDefault()
+    }
+)
