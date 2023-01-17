df98cbb0098b617154366fc7d21089ee1c16a881
Maria Petersen <mau.petersen@gmail.com>
2023-01-17 20:14:53 +0100

    part of cart

--- front/js/product.js
+++ front/js/product.js
@@ -19,7 +19,6 @@ function retrieveProductData() {
             return response.json()}
         })
     .then(function(productData){
-        console.log(productData)
         product = productData.find(function(product){product = product._id === $productId; return product})
         
         itemImage.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}"/>`
@@ -44,6 +43,7 @@ function retrieveProductData() {
 
 retrieveProductData()
 
+//Change product color value when option selected
 productColors.addEventListener('change', function(e){
     document.querySelector('option').setAttribute("value", productColors.value)
     e.preventDefault()
@@ -54,16 +54,66 @@ const addButton = document.getElementById("addToCart")
 const itemQuantity = document.getElementById("quantity")
 
 //Cart object
-let ProductInstance = [
-    ["", "" , 0]
-]
+
+    function Item (id, color, quantity) {
+        this.id = id;
+        this.color = color;
+        this.quantity = quantity
+    }
+
+    let cart = []
+
+   
+    function addItemsToCart() {
+
+        if (cart === null){
+        cart.push(new Item($productId, productColors.value, itemQuantity.value))
+        localStorage.setItem("product", JSON.stringify(cart))
+        return console.log(localStorage)}
+        else
+        {
+        alreadyInCart = localStorage.getItem("product")
+        alreadyInCart = JSON.parse(alreadyInCart)
+        console.log(alreadyInCart)
+ 
+        function findId(product) {
+            product.id === $productId
+            return product.color === productColors.value
+        } //fonctionne 
+
+        let redundantProduct = alreadyInCart.find(findId)
+        console.log(redundantProduct) //fonctionne
+
+        let i = alreadyInCart.findIndex(findId)
+        console.log(i)}
+
+            if (redundantProduct) { //condition fonctionne
+                    let quantityAIng = parseInt(redundantProduct.quantity)
+                    let quantityBIng = parseInt(itemQuantity.value)
+                    alreadyInCart[i] = {id: $productId, color: productColors.value, quantity: quantityAIng + quantityBIng}
+                    cart = alreadyInCart
+                    localStorage.clear() 
+                    localStorage.setItem("product", JSON.stringify(cart))
+                    return console.log(localStorage)
+                }
+        
+         else {
+        cart.push(new Item($productId, productColors.value, itemQuantity.value))
+        localStorage.setItem("product", JSON.stringify(cart))
+        return console.log(localStorage)
+    }
+    }
+
+        // function saveCart(){let stringified = JSON.stringify(cart);
+        //         return localStorage.setItem(prod, stringified)}
+
+        // function getCart (prod) {
+        //         let productsInCart = JSON.parse(localStorage.getItem(prod))
+        //     return cart = productsInCart}
+
 
 //Add to cart functions
-console.log(ProductInstance.id)
-addButton.addEventListener("click", 
-function addToCart(i){
-   ProductInstance.push ([$productId, productColors.value, itemQuantity.value])
-    console.log(ProductInstance)
-    i.preventDefault()
-    }
-)
+
+
+addButton.addEventListener("click", addItemsToCart)
+
